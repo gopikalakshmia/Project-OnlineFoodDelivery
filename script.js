d5c10247b0496c2d9af73ebe29ad301f95023ae4
Gopikalakshmi <gopikalakshmia@gmail.com>
2025-03-16 11:56:42 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+$.ajax({
+  url: "restaurent.json",
+  method: "GET",
+  datatype: "json",
+  success: function (data) {
+    data.forEach((restaurant) => {
+      var restaurantCard= `  <div class="card m-5"  style="width: 20rem;">
+                    <img src="${restaurant.image}" class="card-img-top" alt="${restaurant.name}">
+                    <div class="card-body">
+                        <h5 class="card-title">${restaurant.name}</h5>
+                        <p class="card-text">${restaurant.description}</p>
+                         <a href=${"rest/"+restaurant.id} class="btn btn-primary">Add to Cart</a>
+                    </div>
+                </div>`;
+                $('#restaurant-list').append(restaurantCard);
+    });
+   
+  },
+  error: function (error) {
+   $('#restaurant-list').html('<p>The data is not loaded...Error ocured....</p>')
+  },
+});
